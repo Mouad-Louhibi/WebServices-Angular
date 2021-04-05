7b6eb62d841bc567543a15e405da14f7773c3fdb
mouadlouhibi2000@gmail.com <Math-2019>
2021-04-05 10:23:35 +0100

    Components

--- WebService-Angular/src/app/app.module.ts
+++ WebService-Angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { ListTodoComponent } from './components/list-todo/list-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    ModelsComponent,
+    ServicesComponent,
+    AddTodoComponent,
+    ListTodoComponent
   ],
   imports: [
     BrowserModule,
